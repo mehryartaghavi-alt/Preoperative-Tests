e9ab6d66db109294f7bf48152df1fc84d18163b5
Mehryar <mehryartaghavi@gmail.com>
2026-07-27 09:54:40 +0330

    Update surgical.js

--- surgical.js
+++ surgical.js
@@ -24,3 +24,37 @@ document.getElementById("nextBtn").addEventListener("click", function () {
     window.location.href = "investigations.html";
 
 });
+function showGradeInfo() {
+    alert(
+`SURGICAL GRADE
+
+Minimal
+• Superficial procedures with negligible physiological stress.
+Examples:
+- Skin lesion excision
+- Cataract surgery
+- Diagnostic endoscopy
+
+Minor
+• Low-risk procedures with limited physiological stress.
+Examples:
+- Inguinal hernia repair
+- Breast lumpectomy
+- Arthroscopic knee surgery
+
+Intermediate
+• Moderate physiological stress.
+Examples:
+- Total hip replacement
+- Thyroidectomy
+- Hysterectomy
+
+Major
+• Major physiological stress and/or significant blood loss.
+Examples:
+- CABG
+- Whipple procedure
+- Major liver resection
+- CRS + HIPEC`
+    );
+}
