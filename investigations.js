51900ee6c0861640ee627f1315f91f9a3c8834ae
Mehryar <mehryartaghavi@gmail.com>
2026-08-19 22:40:20 +0330

    Update investigations.js

--- investigations.js
+++ investigations.js
@@ -278,7 +278,7 @@ function R004_CBC() {
 
     if (data.normalizedGrade === "intermediate") {
 
-        addAdvise(test, reason) (
+        addAdvise(
             "CBC",
             "Consider CBC based on clinical assessment and patient-related risk factors."
         );
