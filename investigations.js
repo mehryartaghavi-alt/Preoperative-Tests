2a0419a2d1d6e5dba41ca085a96955e6c80fbcce
Mehryar <mehryartaghavi@gmail.com>
2026-09-03 10:35:15 +0330

    Update investigations.js

--- investigations.js
+++ investigations.js
@@ -1502,11 +1502,7 @@ rules.forEach(rule => rule());
 // ============================================================
 // DEDUPLICATE
 // ============================================================
-const finalResults = uniqueResults(
-    results.required,
-    results.advise,
-    results.actions
-);
+
 results.required = uniqueResults(results.required);
 results.advise = uniqueResults(results.advise);
 results.actions = uniqueResults(results.actions);
